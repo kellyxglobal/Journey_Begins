c97af83dfef77e6bcb9336feb5c64aa49dca9689
Kelechi <kellyxglobal@gmail.com>
2023-08-29 22:10:25 +0100

    Connecting to MongoDB

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 ## - Accessing the server through webbrowser by declaring a route.
 ## - Downlaoding postman and Insomia for AP testing
 ## - Installing nodemon for running without stopping and running the application over again when a change is being made "npm i nodemone -D"
+## - Install mongoose to connect to Mongodb "npm i mongoose"
+## - Visit mongodb.com to register an account, creating a database and connecting my app to DB using the mongodb DB password string generated
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 //Accessing the website via the web browser by decalring route
@@ -10,7 +11,17 @@ app.get('/blogg', (req, res) => {
     res.send('Hello my name is Kelechi')
 })
 
-//Displaying the server page content on the screen via communication protocol
-app.listen(3000, ()=> {
+
+
+//Connect to MongoDB
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://kellyxglobal:Kellxy0303@kellyx.iyxrisn.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Connected to MongoDB')
+    //Displaying the server page content on the screen via communication protocol
+    app.listen(3000, ()=> {
     console.log('My node app is running on port 3000')
+    });
+}).catch(() => {
+    console.log(error)
 })
